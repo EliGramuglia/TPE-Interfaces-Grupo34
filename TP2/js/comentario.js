7701e791d78f87f7e3fa45aa98b0c542a7eec790
EliGramuglia <eliana.gramuglia@gmail.com>
2024-10-07 13:08:17 -0300

    agrego evento para poder puntuar con estrellas el juego, en la sección de los comentarios

---
+++ TP2/js/comentario.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+//Selecciona todas las estrellas
+const estrellas = document.querySelectorAll('.estrella');
+
+for (let i = 0; i < estrellas.length; i++) {
+    //Añade un evento de escucha a cada estrella
+    estrellas[i].addEventListener('click', function(e) {
+        e.preventDefault(); //evita que con href la pag se desplace hacia arriva
+
+        for (let j = 0; j < estrellas.length; j++) {
+            if (j <= i) {
+                //Cambia las estrellas anteriores a amarillas
+                estrellas[j].src = "img/iconos/comentarios-juego/estrella-amarilla.png";
+            } else {
+                //Deja las estrellas posteriores a blanca
+                estrellas[j].src = "img/iconos/comentarios-juego/estrella-blanca.png";
+            }
+        }
+    });
+}

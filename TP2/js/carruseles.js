c255094818e175858d03375cf4f6f9d4de947048
Franco Guidi <119337746+flguidi@users.noreply.github.com>
2024-10-08 12:16:29 -0300

    Parametriza creación de carruseles

--- TP2/js/carruseles.js
+++ TP2/js/carruseles.js
@@ -10,41 +10,47 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
     
-    const tarjetasAccion = [
-        new Tarjeta("Battle Simulator", "ac_battle-simulator.png", 0, 4.7),
-        new Tarjeta("Merge Cannon: Chicken Defense", "ac_chicken-defense.png", 0, 3.8),
-        new Tarjeta("Merge Tanks", "ac_merge-tanks.png", 1299, 4.0),
-        new Tarjeta("Stickman Archero Fight", "ac_stickman-archero-fight.png", 0, 3.2),
-        new Tarjeta("Stickman WWII", "ac_stickman-ww2.png", 2499, 4.9)
-    ];
+    const tarjetas = {
+        accion: [
+            new Tarjeta("Battle Simulator", "ac_battle-simulator.png", 0, 4.7),
+            new Tarjeta("Merge Cannon: Chicken Defense", "ac_chicken-defense.png", 0, 3.8),
+            new Tarjeta("Merge Tanks", "ac_merge-tanks.png", 1299, 4.0),
+            new Tarjeta("Stickman Archero Fight", "ac_stickman-archero-fight.png", 0, 3.2),
+            new Tarjeta("Stickman WWII", "ac_stickman-ww2.png", 2499, 4.9),
+        ],
+        aventura: [
+            new Tarjeta("Age of Tanks", "av_age-of-tanks.png", 0, 4.2),
+            new Tarjeta("Build & Crush", "av_build-and-crush.png", 1399, 4.0),
+            new Tarjeta("Hero 3", "av_hero-3.png", 399, 3.7),
+            new Tarjeta("Ninja Parkour", "av_ninja-parkour.png", 0, 4.5),
+            new Tarjeta("Space Wars", "av_space-wars.png", 0, 4.7)
+        ],
+        clasicos: [
+            new Tarjeta("DuckPark.io", "cl_duckpark-io.png", 599, 4.8),
+            new Tarjeta("Shell Shockers", "cl_shell-shockers.png", 799, 4.3),
+            new Tarjeta("Snake.io", "cl_snake-io.png", 599, 3.2),
+            new Tarjeta("Taming.io", "cl_taming-io.png", 0, 4.9),
+            new Tarjeta("Worm Hunt", "cl_worm-hunt.png", 0, 3.9)
+        ],
+        destacados: [
+            
+        ],
+        futbol: [
 
-    const tarjetasAventura = [
-        new Tarjeta("Age of Tanks", "av_age-of-tanks.png", 0, 4.2),
-        new Tarjeta("Build & Crush", "av_build-and-crush.png", 1399, 4.0),
-        new Tarjeta("Hero 3", "av_hero-3.png", 399, 3.7),
-        new Tarjeta("Ninja Parkour", "av_ninja-parkour.png", 0, 4.5),
-        new Tarjeta("Space Wars", "av_space-wars.png", 0, 4.7)
-    ];
+        ]
+    }
 
-    const tarjetasClasicos = [
-        new Tarjeta("DuckPark.io", "cl_duckpark-io.png", 599, 4.8),
-        new Tarjeta("Shell Shockers", "cl_duckpark-io.png", 799, 4.3),
-        new Tarjeta("Snake.io", "cl_snake-io.png", 599, 3.2),
-        new Tarjeta("Taming.io", "cl_taming-io.png", 0, 4.9),
-        new Tarjeta("Worm Hunt", "cl_worm-hunt.png", 0, 3.9)
-    ];
+    // Para cada categoría, se obtiene su tipo y contenedor, y se crea el carrusel correspondiente
+    document.querySelectorAll('.categoria').forEach(categoria => {
+        const tipo = categoria.dataset.tipo;
+        const contenedorCarrusel = categoria.querySelector('.carrusel');
+        if (tarjetas[tipo]) {
+            crearCarrusel(tarjetas[tipo], contenedorCarrusel);
+        }
+    });
 
-    const tarjetasDestacados = [
-
-    ];
-
-    const tarjetasFutbol = [
-
-    ];
-
-    const carruselAccion = document.querySelector("#carrusel-accion");
-    
-    function crearCarrusel(tarjetas) {
+    // Crea un carrusel de tarjetas en un contenedor dado
+    function crearCarrusel(tarjetas, contenedor) {
         for (let t of tarjetas) {
             // Se crea el contenedor principal de la tarjeta
             const divTarjeta = document.createElement('div');
@@ -66,6 +72,18 @@ document.addEventListener("DOMContentLoaded", () => {
             imgCorazon.src = "img/iconos/tarjetas-juegos/corazon-blanco.png";
             imgCorazon.alt = "Agregar a favoritos";
             btnFavoritos.appendChild(imgCorazon);
+
+            // Se agrega comportamiento al botón de favoritos
+            let activado = false;
+            btnFavoritos.addEventListener('click', () => {
+                if (activado) {
+                    imgCorazon.src = "img/iconos/tarjetas-juegos/corazon-blanco.png";
+                    activado = false;
+                } else {
+                    imgCorazon.src = "img/iconos/tarjetas-juegos/corazon-rosa.png";
+                    activado = true;
+                }
+            });
         
             // Se crea el contenedor de puntaje
             const divPuntaje = document.createElement('div');
@@ -118,47 +136,31 @@ document.addEventListener("DOMContentLoaded", () => {
             divTarjeta.appendChild(divImagen);
             divTarjeta.appendChild(divTexto);
         
-            // Se agrega la tarjeta al carrusel
-            carruselAccion.appendChild(divTarjeta);
-
-            // Se agrega comportamiento al botón de favoritos
-            let activado = false;
-            btnFavoritos.addEventListener('click', () => {
-                if (activado) {
-                    imgCorazon.src = "img/iconos/tarjetas-juegos/corazon-blanco.png";
-                    activado = false;
-                } else {
-                    imgCorazon.src = "img/iconos/tarjetas-juegos/corazon-rosa.png";
-                    activado = true;
-                }
-            });
+            // Se agrega la tarjeta al contenedor del carrusel
+            contenedor.appendChild(divTarjeta);
         }
     }
-
-    crearCarrusel(tarjetasAccion);
 });
 
 
-
 /*
---- HTML de la tarjeta ---
-
-<div id="tarjeta">
-    <div id="contenedor-imagen-tarjeta">
+----------------------------- Ejemplo de tarjeta generada en HTML -----------------------------
+<div class="tarjeta">
+    <div class="contenedor-imagen-tarjeta">
         <img src="img/cards/ac_chicken-defense.png" alt="Juego de acción">
-        <button id="icono-corazon-favoritos">
+        <button class="icono-corazon-favoritos">
             <img src="img/iconos/tarjetas-juegos/corazon-blanco.png"
                 alt="Corazón para clickear los juegos favoritos">
         </button>
-        <div id="puntaje-juego">
+        <div class="puntaje-juego">
             <img src="img/iconos/tarjetas-juegos/estrella.png" alt="Estrella de puntaje juego">
             <p>0.0</p>
         </div>
     </div>
-    <div id="contenedor-texto-tarjeta">
+    <div class="contenedor-texto-tarjeta">
         <div>
-            <p id="tarjeta-nombre-juego">Nombre del juego</p>
-            <p id="tarjeta-precio-juego">$000.00</p>
+            <p class="tarjeta-nombre-juego">Nombre del juego</p>
+            <p class="tarjeta-precio-juego">$000.00</p>
         </div>
         <button class="iconos-tarjetas-play-carrito">
             <img src="img/iconos/tarjetas-juegos/icono-play.png" alt="Icono play">

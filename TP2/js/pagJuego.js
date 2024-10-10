33dee3adc7fb2111f7c9ce071af6f44d759a0584
Julieta Simos <juliet.simos@gmail.com>
2024-10-10 15:37:49 -0300

    agregado estilos al nav de la pantalla del juego

---
+++ TP2/js/pagJuego.js
@@ -0,0 +1,5 @@
+"use strict"
+
+//seleccionar la img y cambiarla por otra si hacen un click, darle un id tambine 
+let contenedorValoracion = document.querySelectorAll('div.caja-valoracion-juego');
+

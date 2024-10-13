80e7d99be376d7a0c7e7c2f3cf1af2048d5dfb69
Julieta Simos <juliet.simos@gmail.com>
2024-10-13 16:49:49 -0300

    agregada funcion para verificar que los input esten completos antes de redirigir al home

--- TP2/js/formulario.js
+++ TP2/js/formulario.js
@@ -9,7 +9,7 @@ const formRegistro = document.getElementById('formRegistro');
 opcIngreso.addEventListener('click', () => cambiarCaja(formIngreso, formRegistro));
 opcRegistro.addEventListener('click', () => cambiarCaja(formRegistro, formIngreso));
 
-function cambiarCaja(box1,box2){
+function cambiarCaja(box1, box2) {
     //muestro el box1
     box1.classList.remove("oculto");
     //escondo el box2
@@ -23,55 +23,68 @@ const opciones = document.querySelectorAll('.nav-registro h2');
 opciones.forEach(opcion => {
     opcion.addEventListener('click', () => {
         opciones.forEach(opc => opc.classList.remove('active')),
-        opciones.forEach(opc => opc.classList.add('inactivo'));
+            opciones.forEach(opc => opc.classList.add('inactivo'));
 
-    opcion.classList.add("active");
+        opcion.classList.add("active");
     });
 });
 
 
-const btnLogin = document.getElementById('btn-login');
-const btnRegistro = document.getElementById('btn-registro');
-
-btnRegistro.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    window.location.href = 'login.html';
-})
-
-btnLogin.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    window.location.href = 'index.html';
-})
-
-
-//funcionalidades para validacion de inputs
-//arreglar problema de que se envia igual los btn, no importa el preventDefault
+//  funcionalidad para btn-principales del registro
 const formularioIngreso = document.querySelector('#formIngreso form');
 const formularioRegistro = document.querySelector('#formRegistro form');
 
+const btnRegistro = document.getElementById('btn-registro');
+const btnLogin = document.getElementById('btn-login');
 
+btnLogin.addEventListener('click', (e) => {
+    e.preventDefault();
+    let inputForm = document.querySelectorAll('#formIngreso form input[required]');
+    advertenciaInput(inputForm);
+    if (validandoFormulario(inputForm)) {
+        window.location.href = 'index.html';
+    }
+})
+btnRegistro.addEventListener('click', (e) => {
+    e.preventDefault();
+    let inputForm = document.querySelectorAll('#formRegistro form input[required]');
+    advertenciaInput(inputForm);
+    if (validandoFormulario(inputForm)) {
+        window.location.href = 'index.html';
+    }
+})
+
+function validandoFormulario(inputs) {
+    let flag = true;
+    for (const input of inputs) { 
+        //me fijo si los input tipo checkbox estan marcados
+        if (input.type === 'checkbox' && !input.checked) {
+            flag = false;
+            break;
+            //me fijo si el resto de los input estan con contenido
+        } else if (input.type !== 'checkbox' && input.value === '') {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
+
+
+//funcionalidades para advertencia de inputs
 function advertenciaInput(inputForm) {
     inputForm.forEach(input => {
-        if(input.value === ""){
+        if (input.value === "") {
             input.classList.add('inputFaltante');
-        }else{
+        } else {
             input.classList.remove('inputFaltante');
         }
-})
+    })
 }
-function prevenirEnvio(e) {
-    e.preventDefault();
-}
-btnLogin.addEventListener('click', (e) => {
-    let inputForm = document.querySelectorAll('#formIngreso form input');
-    e.preventDefault();
-    advertenciaInput(inputForm);
-});
 
-btnRegistro.addEventListener('click', (e) => {
-    let inputForm = document.querySelectorAll('#formRegistro form input');
-    e.preventDefault();
-    advertenciaInput(inputForm);
-});
+
+
+
+
+
+

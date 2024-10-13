fbd5ff2ab6f698249fc5fbe7ab972d290b120626
Franco Guidi <119337746+flguidi@users.noreply.github.com>
2024-10-13 11:08:07 -0300

    Agrega toggle en caja de instrucciones

--- TP2/js/paginaJuego.js
+++ TP2/js/paginaJuego.js
@@ -1,29 +1,33 @@
 "use strict"
 
 document.addEventListener('DOMContentLoaded', () => {
-    let listaItemsCompartir = document.querySelector('#lista-items-compartir');
-    let btnCompartir = document.querySelector('#btn-compartir-juego');
-    btnCompartir.addEventListener('click', () =>{
+    // ---- Corazón ----
+    const btnCorazon = document.getElementById('opc-corazon');
+    const imgEstados = ['img/iconos/comentarios-juego/icono-corazon.png', 'img/iconos/tarjetas-juegos/corazon-rosa.png'];
+    let indice = 0;
+    btnCorazon.addEventListener('click', () => {
+        indice = (indice + 1) % imgEstados.length;
+        btnCorazon.src = imgEstados[indice];
+    });
+
+    // ---- Botón Compartir ----
+    const listaItemsCompartir = document.querySelector('#lista-items-compartir');
+    const btnCompartir = document.querySelector('#btn-compartir-juego');
+    btnCompartir.addEventListener('click', () => {
         listaItemsCompartir.classList.toggle('hidden');
     });
+    
+    // ---- Botón de instrucciones ----
+    const btnInstrucciones = document.getElementById('opc-instrucciones');
+    const btnCerrar = document.getElementById('btn-cerrar-instrucciones');
+    const caja = document.getElementById('caja-instrucciones');
+    
+    btnInstrucciones.addEventListener('click', () => {
+        caja.classList.toggle('visible');
+    });
+
+    btnCerrar.addEventListener('click', () => {
+        caja.classList.remove('visible');
+    });
 });
-const botonCorazon = document.getElementById('opc-corazon');
-const imgEstados = ['img/iconos/comentarios-juego/icono-corazon.png', 'img/iconos/tarjetas-juegos/corazon-rosa.png'];
-let indice = 0;
-
-botonCorazon.addEventListener('click', () => {
-    indice = (indice + 1) % imgEstados.length;
-    botonCorazon.src = imgEstados[indice];
-});
-
-//------------------ funcionalidad de btn instrucciones
-const btnInstrucciones = document.getElementById('opc-instrucciones');
-const caja = document.getElementById('caja-instrucciones');
-const btbCerrar = document.getElementById('btn-cerrar-instrucciones');
-
-
-btnInstrucciones.addEventListener('click', () =>{
-    console.log('dentro de la funcion de btn instrucciones')
-    caja.style.animation = 'mostrarInstrucciones .2s linear forwards';
-
-})
+    

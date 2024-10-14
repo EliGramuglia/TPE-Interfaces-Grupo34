d97bf36db2f70a613676fc262270c3e8e3cb8be9
Franco Guidi <119337746+flguidi@users.noreply.github.com>
2024-10-14 12:20:54 -0300

    Agrega funcionalidad a flechas en imagen hero

--- TP2/js/imagenHero.js
+++ TP2/js/imagenHero.js
@@ -17,42 +17,57 @@ document.addEventListener('DOMContentLoaded', () => {
         new Juego('Rabbids', 'Juegos populares', 'rabbids.jpg'),
     ];
 
-    // Contenedor de imagen hero
+    // Elementos HTML
     const sectionHero = document.querySelector('#imagen-hero');
-
-    // Flechas
+    const titulo = document.querySelector('#info-juego h1');
+    const categoria = document.querySelector('#info-juego p');
     const flechaIzq = document.querySelector('.hero-flecha-izquierda');
     const flechaDer = document.querySelector('.hero-flecha-derecha');
+    
+    // Visibilidad de flechas
     sectionHero.addEventListener('mouseenter', () => {
         flechaIzq.classList.add('visible');
         flechaDer.classList.add('visible');
     });
+
     sectionHero.addEventListener('mouseleave', () => {
         flechaIzq.classList.remove('visible');
         flechaDer.classList.remove('visible');
     });
 
-    // Tiempo de espera antes de activar la animación
-    setTimeout(() => {
+    // Cambio de imágenes
+    const frecuencia = 10000; // Frecuencia de cambio de imágenes (ms)
+    let intervalo; // Intervalo de cambio de imágenes
+    let indice = 0; // Índice de imagen
+
+    flechaIzq.addEventListener('click', () => {
+        indice = (indice - 1 + juegos.length) % juegos.length;
+        cambiarImagen();
         animarHero();
-    }, 7000);
+    });
+
+    flechaDer.addEventListener('click', () => {
+        indice = (indice + 1) % juegos.length;
+        cambiarImagen();
+        animarHero();
+    });
 
     function animarHero() {
-        // Generación de imagen hero
-        const titulo = document.querySelector('#info-juego h1');
-        const categoria = document.querySelector('#info-juego p');
-        const frecuencia = 10000; // Frecuencia de cambio en milisegundos
-        const cantidadJuegos = juegos.length;
-
-        let i = 0;
-        const intervalo = setInterval(() => {
-            if (i >= cantidadJuegos) {
-                i = 0;
-            }
-            titulo.textContent = juegos[i].titulo
-            categoria.textContent = juegos[i].categoria;
-            sectionHero.style.backgroundImage = `url('img/hero/${juegos[i].img}')`;
-            i++;
+        clearInterval(intervalo); // Cada vez que se llama, se reinicia el intervalo
+        intervalo = setInterval(() => {
+            indice = (indice + 1) % juegos.length;
+            cambiarImagen();
         }, frecuencia);
     }
+
+    function cambiarImagen() {
+        titulo.textContent = juegos[indice].titulo
+        categoria.textContent = juegos[indice].categoria;
+        sectionHero.style.backgroundImage = `url('img/hero/${juegos[indice].img}')`;
+    }
+
+    // Luego de un tiempo de espera, se dispara la animación
+    setTimeout(() => { 
+        animarHero();
+    }, 7000);
 });

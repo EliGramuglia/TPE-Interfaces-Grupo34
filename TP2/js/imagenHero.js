1e6b70cbd7b4e57596d9549a08e50b911bda2353
Franco Guidi <119337746+flguidi@users.noreply.github.com>
2024-10-14 19:23:17 -0300

    Añade animación en el hero al cambiar de juego

--- TP2/js/imagenHero.js
+++ TP2/js/imagenHero.js
@@ -42,13 +42,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     flechaIzq.addEventListener('click', () => {
         indice = (indice - 1 + juegos.length) % juegos.length;
-        cambiarImagen();
+        cambiarJuego();
         animarHero();
     });
 
     flechaDer.addEventListener('click', () => {
         indice = (indice + 1) % juegos.length;
-        cambiarImagen();
+        cambiarJuego();
         animarHero();
     });
 
@@ -56,14 +56,26 @@ document.addEventListener('DOMContentLoaded', () => {
         clearInterval(intervalo); // Cada vez que se llama, se reinicia el intervalo
         intervalo = setInterval(() => {
             indice = (indice + 1) % juegos.length;
-            cambiarImagen();
+            cambiarJuego();
         }, frecuencia);
     }
 
-    function cambiarImagen() {
-        titulo.textContent = juegos[indice].titulo
-        categoria.textContent = juegos[indice].categoria;
-        sectionHero.style.backgroundImage = `url('img/hero/${juegos[indice].img}')`;
+    function cambiarJuego() {
+        // Reinicio de animaciones
+        sectionHero.classList.remove('difuminar-fondo');
+        categoria.classList.remove('fade-out-in');
+        titulo.classList.remove('desplazar');
+        sectionHero.offsetWidth;
+        sectionHero.classList.add('difuminar-fondo');
+        categoria.classList.add('fade-out-in');
+        titulo.classList.add('desplazar');
+
+        // Delay para sincronizar cambio de imagen con efecto de desenfoque
+        setTimeout(() => {
+            titulo.textContent = juegos[indice].titulo;
+            categoria.textContent = juegos[indice].categoria;
+            sectionHero.style.backgroundImage = `url('img/hero/${juegos[indice].img}')`;
+        }, 500);
     }
 
     // Luego de un tiempo de espera, se dispara la animación

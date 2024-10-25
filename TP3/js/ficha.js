fbf0c6e5c6a7437aa210dd27b9650fee6126da89
Franco Guidi <119337746+flguidi@users.noreply.github.com>
2024-10-25 17:22:17 -0300

    Se agrega funcionalidad 'Arrastrar & Soltar'

--- TP3/js/ficha.js
+++ TP3/js/ficha.js
@@ -4,24 +4,28 @@
  * Esta clase representa una ficha del juego con su posición x-y, radio, velocidad x-y y color.
  */
 export class Ficha {
-    constructor(x, y, radio, vx, vy, friccion, color) {
+    constructor(x, y, color) {
         this.x = x;
         this.y = y;
-        this.radio = radio;
-        this.vx = vx;
-        this.vy = vy;
-        this.friccion = friccion;
         this.color = color;
+        this.radio = 36;
+        this.vx = 0;
+        this.vy = 1;
+        this.friccion = 0.6; // Fricción que disminuye fuerza de rebote
+        this.gravedad = 0.3; // Gravedad que afecta caída
+        this.seleccionada = false;
     }
 
-    actualizar(canvas, gravedad) {
-        this.y += this.vy;
-        this.vy += gravedad;
-
-        if (this.y + this.radio > canvas.height) {
-            this.y = canvas.height - this.radio;
-            this.vy *= -1;
-            this.vy *= this.friccion;
+    actualizar(canvas) {
+        if (this.vy != 0 && !this.seleccionada) {
+            this.vy += this.gravedad;
+            this.y += this.vy;
+            
+            if (this.y + this.radio > canvas.height) {
+                this.y = canvas.height - this.radio;
+                this.vy *= -1;
+                this.vy *= this.friccion;
+            }
         }
     }
 
@@ -31,5 +35,14 @@ export class Ficha {
         ctx.fillStyle = this.color;
         ctx.fill();
         ctx.closePath();
+    }
+
+    sobreCoordenadasMouse(coordenadasMouse) {
+        const distX = coordenadasMouse.x - this.x;
+        const distY = coordenadasMouse.y - this.y;
+        const distancia = Math.sqrt(distX * distX + distY * distY);
+        
+        // Si la distancia es menor o igual al radio, la ficha está siendo seleccionada
+        return distancia <= this.radio;
     }
 }
--- TP3/js/juego.js
+++ TP3/js/juego.js
@@ -9,31 +9,78 @@ import { Tablero } from './Tablero.js';
  */
 export class Juego {
     constructor(canvasId, width, height) {
+        // Canvas
         this.canvas = document.querySelector(canvasId);
         this.ctx = this.canvas.getContext('2d');
         this.width = width;
         this.height = height;
         this.canvas.width = this.width;
         this.canvas.height = this.height;
+        this.colorFondo = '#222222';
+
+        // Coordenadas del cursor del mouse
+        this.mouse = {
+            x: undefined,
+            y: undefined
+        };
+        this.offset = {
+            x: undefined,
+            y: undefined
+        }
 
         // Elementos del juego
-        this.gravedad = 0.3;
         this.tablero = new Tablero();
-        this.fichas = [];
-        this.cantFichas = 20;
         this.j1 = new Jugador();
         this.j2 = new Jugador();
+        this.fichas = [];
+        this.cantFichas = 20;
+        this.fichaSeleccionada; // Determina qué ficha está siendo arrastrada
 
         this.inicializar();
     }
 
     inicializar() {
+        // Fichas
         for (let i = 0; i < this.cantFichas; i++) {
-            let tono = Math.random() * 360;
+            let yRandom = Math.random() * 400 + 20;
+            let tonoRandom = Math.random() * 360;
+            this.fichas.push(new Ficha(i * 50, yRandom, `hsla(${tonoRandom}, 40%, 50%, 0.8)`));
+        }
 
-            let y = Math.random() * 400 + 20;
-    
-            this.fichas.push(new Ficha(i * 50, y, 36, 0, 1, 0.6, `hsla(${tono}, 40%, 50%, 0.8)`));
+        // Event listeners
+        this.canvas.addEventListener('mousedown', (e) => {
+            this.mouse = this.obtenerCoordenadasMouse(e);
+            this.fichaSeleccionada = this.fichas.find(f => f.sobreCoordenadasMouse(this.mouse));
+            
+            if (this.fichaSeleccionada) {
+                this.fichaSeleccionada.seleccionada = true;
+                this.arrastrando = true;
+                this.offset.x = this.mouse.x - this.fichaSeleccionada.x;
+                this.offset.y = this.mouse.y - this.fichaSeleccionada.y;
+            }
+        });
+
+        this.canvas.addEventListener('mousemove', (e) => {
+            if (this.fichaSeleccionada) {
+                this.mouse = this.obtenerCoordenadasMouse(e);
+                this.fichaSeleccionada.x = this.mouse.x - this.offset.x;
+                this.fichaSeleccionada.y = this.mouse.y - this.offset.y;
+            }
+        });
+
+        this.canvas.addEventListener('mouseup', (e) => {
+            if (this.fichaSeleccionada) {
+                this.fichaSeleccionada.seleccionada = false;
+                this.fichaSeleccionada = undefined;
+            }
+        });
+    }
+
+    obtenerCoordenadasMouse(evento) {
+        const rect = this.canvas.getBoundingClientRect()
+        return {
+            x: evento.clientX - rect.left,
+            y: evento.clientY - rect.top
         }
     }
 
@@ -58,7 +105,7 @@ export class Juego {
 
     dibujar() {
         // Fondo
-        this.ctx.fillStyle = 'lightblue';
+        this.ctx.fillStyle = this.colorFondo;
         this.ctx.fillRect(0, 0, this.width, this.height);
 
         // Fichas

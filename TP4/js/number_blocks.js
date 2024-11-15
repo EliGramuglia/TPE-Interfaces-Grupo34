a656033354d5c56438e6e29b8f7a4d1361b00072
Julieta Simos <juliet.simos@gmail.com>
2024-11-15 03:58:51 -0300

    agregadas img para el hero, posicionados provisionalmente en la pantalla, cambiada ruta de img de la sube en el prefooter, comenzada a pensarse animacion para la segunda seccion

---
+++ TP4/js/number_blocks.js
@@ -0,0 +1,19 @@
+"use strict"
+
+//elementos necesarios para las animaciones
+const pagina = document.getElementById('contenedor-pagina');
+const hero = document.getElementById('contenedor-hero-visual');
+
+
+
+// const wrapper = document.getElementById("wrapper");
+// const paralx0 = document.getElementById("paralx0");
+// const paralx1 = document.getElementById("paralx1");
+// const paralx2 = document.getElementById("paralx2");
+
+// wrapper.onscroll = function () {
+//   let y = wrapper.scrollTop;
+//   paralx0.style.top = 1800 - y * 2.65 + "px";
+//   paralx1.style.top = 1200 - y * 0.75 + "px";
+//   paralx2.style.top = 800 + y * 0.5 + "px";
+// };

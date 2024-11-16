688a870c5d070845d4e63ef26bbd150515a6e18f
Julieta Simos <juliet.simos@gmail.com>
2024-11-16 04:47:18 -0300

    siguiendo con la animacion de la seccion 2: presentacion textual

--- TP4/js/number_blocks.js
+++ TP4/js/number_blocks.js
@@ -2,7 +2,10 @@
 
 //elementos necesarios para las animaciones
 const pagina = document.getElementById('contenedor-pagina');
-const hero = document.getElementById('contenedor-hero-visual');
+//guardo las secciones de la pag
+const hero = document.getElementById('contenedor-hero-visual'); //sec 1: hero
+const introTextual = document.getElementById('contenedor-presentacion-textual'); //sec 2: intro textual
+
 
 
 
@@ -17,3 +20,26 @@ const hero = document.getElementById('contenedor-hero-visual');
 //   paralx1.style.top = 1200 - y * 0.75 + "px";
 //   paralx2.style.top = 800 + y * 0.5 + "px";
 // };
+
+//capto el scroll de toda la pantalla
+window.addEventListener('scroll', () => {
+    let scrollTop = window.scrollY; 
+
+        let topSec_2 = introTextual.getBoundingClientRect().top;
+        // console.log("Posición actual del scroll:"+ topSec_2);
+        previousScrollTop = scrollTop;
+        if(scrollTop >= topSec_2 - topSec_2 / 2){
+            console.log('dentor de la seccion, se activa animacion')
+        }
+});
+
+let previousScrollTop = 0; // Variable para almacenar la posición anterior del scroll
+
+// Tus funciones para mostrar/ocultar el menú o realizar otras acciones
+function showIntroTextual() {
+    // se animan los elementos de esta seccion
+}
+
+function show3Videos() {
+    // se animan los elementos de esta seccion
+}
